2ebc578fb16cd2e20b8857891a2ff836709deab2
Brian Rossetti <brossetti1@gmail.com>
2022-07-27 22:06:33 -0600

    wip

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ export { default as SharesABI } from '../abi/contracts/SharesERC20.sol/Shares.js
 export { default as PosterABI } from '../abi/contracts/tools/Poster.sol/Poster.json';
 export { default as TributeMinionABI } from '../abi/contracts/tools/TributeMinion.sol/TributeMinion.json';
 
-export { BaalFactory } from './types/BaalFactory';
-export { BaalSummonerFactory } from './types/BaalSummonerFactory';
-export { LootFactory } from './types/LootFactory';
-export { SharesFactory } from './types/SharesFactory';
-export { PosterFactory } from './types/PosterFactory';
-export { TributeMinionFactory } from './types/TributeMinionFactory';
+export { Baal__factory as BaalFactory } from './types/factories/contracts/Baal.sol/Baal__factory';
+export { BaalSummoner__factory as BaalSummonerFactory } from './types/factories/contracts/Baal.sol/BaalSummoner__factory';
+export { Loot__factory as LootFactory } from './types/factories/contracts/LootERC20.sol/Loot__factory';
+export { Shares__factory as SharesFactory } from './types/factories/contracts/SharesERC20.sol/Shares__factory';
+export { Poster__factory as PosterFactory } from './types/factories/contracts/tools/Poster__factory';
+export { TributeMinion__factory as TributeMinionFactory } from './types/factories/contracts/tools/TributeMinion.sol/TributeMinion__factory';
